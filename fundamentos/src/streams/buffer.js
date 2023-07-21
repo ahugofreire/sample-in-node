686f092d679efcffea9a3027242077f386553cd0
Hugo Freire <ahugo.freire@gmail.com>
2023-07-21 17:20:54 -0300

    add buffer example

---
+++ fundamentos/src/streams/buffer.js
@@ -0,0 +1,5 @@
+const buf = Buffer.from('Olá mundo!')
+
+console.log(buf) // Hexadecimal cada caractere é representado um letra ou número;
+console.log(buf.toString()) // Representação em String;
+console.log(buf.toJSON()) // Representação em Decimal dentro do JSON;
